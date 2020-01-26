6581654f9f1c010e5d5bd0cba18319092f9f7d26
Gabriel Santos <barricaspt@gmail.com>
2020-01-26 19:43:30 +0000

    Remove old stuff

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Factory_1 = require("./Bot/Factory");
-const ConnectionProtocol_1 = require("./Bot/ConnectionProtocol");
-const configuration = {
-    host: '192.168.1.200',
-    queryport: 10011,
-    serverport: 9987,
-    nickname: 'TS BOT',
-    protocol: ConnectionProtocol_1.ConnectionProtocol.RAW,
-    username: 'bot',
-    password: 'zFScOXvI'
-};
 const botFactory = new Factory_1.Factory();
 botFactory.create('testserver')
     .then(bot => {
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
 import { Factory } from './Bot/Factory';
-import { ConnectionProtocol } from './Bot/ConnectionProtocol';
-
-const configuration = {
-    host: '192.168.1.200',
-    queryport: 10011,
-    serverport: 9987,
-    nickname: 'TS BOT',
-    protocol: ConnectionProtocol.RAW,
-    username: 'bot',
-    password: 'zFScOXvI'
-};
 
 const botFactory = new Factory();
 
