5d140e51d5f51cadfd1d32675296357cf3aa724f
Gabriel Santos <barricaspt@gmail.com>
2020-02-04 00:17:11 +0000

    Clean dist folder. Add rimraf utility, move typescript to dev deps, add script to clean dist folder when compiling.

--- dist/Database/LocalDatabase.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class LocalDatabase {
-}
-exports.LocalDatabase = LocalDatabase;
-//# sourceMappingURL=LocalDatabase.js.map
