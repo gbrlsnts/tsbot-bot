e033677d6e6fa75a023d94261b6276f31da068c0
Gabriel Santos <barricaspt@gmail.com>
2020-09-02 00:52:11 +0100

    Add optional rootChannelId to validation rules

--- dist/Validation/UserChannel/UserChannelValidationRules.js
+++ dist/Validation/UserChannel/UserChannelValidationRules.js
@@ -16,22 +16,26 @@ exports.channelProps = {
     })),
     properties: Joi.object().keys({
         audio: Joi.object().keys({
-            codec: Joi.string().required().valid(...Object.values(Types_1.BotCodec)),
+            codec: Joi.string()
+                .required()
+                .valid(...Object.values(Types_1.BotCodec)),
             quality: Joi.number().required().integer().min(0).max(10),
-        })
+        }),
     }),
 };
 exports.createChannel = {
     ...exports.zone,
     owner: Joi.number().min(1),
     group: Joi.number().min(1),
-    channels: Joi.array().required().items(Joi.object().keys({
+    channels: Joi.array()
+        .required()
+        .items(Joi.object().keys({
         name: Joi.string().required().min(0).max(40),
         password: Joi.string().min(1),
         channels: Joi.link('...').required(),
-        ...exports.channelProps
+        ...exports.channelProps,
     })),
-    ...exports.channelProps
+    ...exports.channelProps,
 };
 exports.channelId = {
     channelId: Joi.number().required().min(1),
@@ -43,5 +47,6 @@ exports.createSubChannel = {
 exports.deleteChannel = {
     ...exports.zone,
     ...exports.channelId,
+    rootChannelId: Joi.number().optional().min(1),
 };
 //# sourceMappingURL=UserChannelValidationRules.js.map
